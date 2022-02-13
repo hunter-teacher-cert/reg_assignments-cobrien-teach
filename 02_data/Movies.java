395310416e564c24a3843d2bcbe1fb26fb08001e
Chris O'Brien <cobrien528@gmail.com>
2022-02-13 17:20:01 -0500

    week 2 data assignment

---
+++ 02_data/Movies.java
@@ -0,0 +1,44 @@
+public class Movies{
+  public static int get_max_index(double[] arr){
+    int index = 0;
+    for (int i=1; i<arr.length; i++){
+      if (arr[i]>arr[index]){
+        index = i;
+      }
+    }
+    return index;
+  }
+  public static String biggest_budget(String[] titles, double[] budget){
+    int index = get_max_index(budget);
+    return titles[index];
+  }
+  public static String biggest_gross(String[] titles, double[] grosses){
+    int gross_index = get_max_index(grosses);
+    return titles[gross_index];
+  }
+  public static String most_profitable(String[] titles,double[] budget, double[] grosses){
+    //calculates profitability as a proportion of budget
+    double[] profitability = new double[titles.length];
+    for (int i = 0; i<titles.length; i++){
+      double profit = (grosses[i] - budget[i]) / budget[i];
+      profitability[i] = profit;
+    }
+    int max_profit = get_max_index(profitability);
+
+    return titles[max_profit];
+  }
+
+  public static void main(String[] args){
+    //The title budget and grosses for five movies, matched by index.
+    String[] titles = { "Barry Lyndon", "Zodiac", "The Big Lebowski", "Burn After Reading", "Alien"};
+    double[] budget = {11, 65, 15,37, 11}; //in millions
+    double[] grosses ={20.2, 84.8, 46.8,163.7, 106.2}; //in millions
+
+    System.out.println(biggest_budget(titles,budget));
+    System.out.println(biggest_gross(titles,grosses));
+    System.out.println(most_profitable(titles,budget,grosses));
+
+
+
+  }
+}
---
+++ 02_data/README.md
@@ -0,0 +1,6 @@
+# Assignment representing data with Arrays
+Use imdb.com to look up the budget and international gross of your five favorite movies.  Represent this information in a series of arrays.
+
+Write methods that return: The highest grossing, lowest grossing, and most profitable movie of the ones you picked.
+
+See Movies.java for a representative submission.
